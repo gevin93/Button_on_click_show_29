781f591e5846ff72b294fa8b036547a181010efb
gevin93 <gevindumadanayake@gmail.com>
2016-06-29 19:33:36 +0530

    commented

--- app/src/main/java/com/example/gevin93/button_on_click_show/MainActivity.java
+++ app/src/main/java/com/example/gevin93/button_on_click_show/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+//testing  whether this works
 
 public class MainActivity extends AppCompatActivity {
 
