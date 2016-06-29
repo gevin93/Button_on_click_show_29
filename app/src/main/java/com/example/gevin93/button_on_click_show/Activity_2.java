31068463e88b99fdf1542c1996e514949f5ee30a
gevin93 <gevindumadanayake@gmail.com>
2016-06-29 19:29:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gevin93/button_on_click_show/Activity_2.java
@@ -0,0 +1,31 @@
+package com.example.gevin93.button_on_click_show;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_2);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
+
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/gevin93/button_on_click_show/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.gevin93.button_on_click_show;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button sendMessage;  //button variable
+
+    public final static String EXTRA_MESSAGE = "com.example.gevin93.button_on_click_show.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendMessage = (Button) findViewById(R.id.button1); //setting up the button in the activity_main layout to on click
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Activity_2.class); //the message show to the next layout
+
+                EditText editText = (EditText) findViewById(R.id.edit_message); //calling R
+                String message = editText.getText().toString(); //converting the message in the text field to string and save in a variable
+
+                intent.putExtra(EXTRA_MESSAGE, message);
+
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show(); //toast to show a little pop up message to show the message you entered.
+                Toast.makeText(MainActivity.this,"Hello!", Toast.LENGTH_LONG).show(); //toast to a pop up saying hello
+                    startActivity(intent);
+
+            }
+            });
+
+    }
+}
